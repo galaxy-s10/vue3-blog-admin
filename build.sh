f25867d5d34f2d43b81f830659dd9b08cf500bd6
shuisheng <2274751790@qq.com>
2022-04-24 00:59:31 +0800

    fix:修改build.sh

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+###
+# Author: shuisheng
+# LastEditors: shuisheng
+# LastEditTime: 2022-04-23 01:46:10
+# Github: https://github.com/galaxy-s10
+# Email: 2274751790@qq.com
+# Description:
+# Date: 2022-04-23 01:41:23
+###
+###
+# Author: shuisheng
+# Email: 2274751790@qq.com
+# Github: https://github.com/galaxy-s10
+# Date: 2022-01-16 04:15:31
+# LastEditTime: 2022-04-09 03:40:40
+# Description:
+###
+
+# 约定$1为任务名, $2为环境, $3为Jenkins工作区, $4为端口号
+JOBNAME=$1 # 注意: JOBNAME=$1,这个等号左右不能有空格！
+ENV=$2
+WORKSPACE=$3
+PORT=$4
+PUBLICDIR=/node
+
+echo 删除node_modules:
+rm -rf node_modules
+
+echo 查看npm版本:
+npm -v
+
+echo 设置npm淘宝镜像:
+npm config set registry http://registry.npm.taobao.org/
+
+echo 查看当前npm镜像:
+npm get registry
+
+if ! type yarn >/dev/null 2>&1; then
+  echo 'yarn未安装,先全局安装yarn'
+  npm i yarn -g
+else
+  echo 'yarn已安装'
+fi
+
+echo 查看yarn版本:
+yarn -v
+
+echo 设置yarn淘宝镜像:
+yarn config set registry https://registry.npm.taobao.org
+
+echo 查看当前yarn镜像:
+yarn config get registry
+
+echo 开始安装依赖:
+yarn install
+
+if [ $ENV = 'beta' ]; then
+  echo 开始构建测试环境:
+elif [ $ENV = 'preview' ]; then
+  echo 开始构建预发布环境:
+elif [ $ENV = 'prod' ]; then
+  echo 开始构建正式环境:
+else
+  echo 开始构建$ENV环境:
+fi
+
+# npx cross-env VUE_APP_RELEASE_PROJECT_NAME=$JOBNAME VUE_APP_RELEASE_PROJECT_ENV=$ENV webpack --config ./config/webpack.common.ts --env production
+npx vue-tsc --noEmit && npx vite build
