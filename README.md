65de1c2b72521bb1544003bdd9986b9baa5826e8
shuisheng <2274751790@qq.com>
2023-04-22 20:48:29 +0800

    docs: readme

--- README.md
+++ README.md
@@ -8,6 +8,58 @@
 
 # 项目截图
 
+登录：
+
+![https://resource.hsslive.cn/image/e8241c8c3c9b259a0cc61ca316482f93.webp](https://resource.hsslive.cn/image/e8241c8c3c9b259a0cc61ca316482f93.webp)
+
+控制台：
+
+![https://resource.hsslive.cn/image/d9068218a9c6558b3db07266b554fc7a.webp](https://resource.hsslive.cn/image/d9068218a9c6558b3db07266b554fc7a.webp)
+
+角色列表：
+
+![https://resource.hsslive.cn/image/b7466a76a9cb2b104ec514df7d35b893.webp](https://resource.hsslive.cn/image/b7466a76a9cb2b104ec514df7d35b893.webp)
+
+角色树：
+
+![https://resource.hsslive.cn/image/804590ba404c7eda28dec55ce8094689.webp](https://resource.hsslive.cn/image/804590ba404c7eda28dec55ce8094689.webp)
+
+权限列表：
+
+![https://resource.hsslive.cn/image/a95304ea89428ce356a74d05570bf78e.webp](https://resource.hsslive.cn/image/a95304ea89428ce356a74d05570bf78e.webp)
+
+权限树：
+
+![https://resource.hsslive.cn/image/bf76360b9f52c12598dacad1f7cc4a8d.webp](https://resource.hsslive.cn/image/bf76360b9f52c12598dacad1f7cc4a8d.webp)
+
+用户列表：
+
+![https://resource.hsslive.cn/image/f43a658965af056418f8507628fbf157.webp](https://resource.hsslive.cn/image/f43a658965af056418f8507628fbf157.webp)
+
+日志列表：
+
+![https://resource.hsslive.cn/image/8185a484698664f74b06d54bb56feac7.webp](https://resource.hsslive.cn/image/8185a484698664f74b06d54bb56feac7.webp)
+
+友链列表：
+
+![https://resource.hsslive.cn/image/6e888d01adb02098136f552e93b64e39.webp](https://resource.hsslive.cn/image/6e888d01adb02098136f552e93b64e39.webp)
+
+文章列表：
+
+![https://resource.hsslive.cn/image/92200b687305c22b6b382e748c0029f1.webp](https://resource.hsslive.cn/image/92200b687305c22b6b382e748c0029f1.webp)
+
+发布文章：
+
+![https://resource.hsslive.cn/image/520d6e3af2f0aa389d7a802cecb29f77.webp](https://resource.hsslive.cn/image/520d6e3af2f0aa389d7a802cecb29f77.webp)
+
+点赞列表：
+
+![https://resource.hsslive.cn/image/3f0bbd0eb5f70511f41f30cb57c32a16.webp](https://resource.hsslive.cn/image/3f0bbd0eb5f70511f41f30cb57c32a16.webp)
+
+评论列表：
+
+![https://resource.hsslive.cn/image/c762c97e87859eb6d1b15c853a026c6d.webp](https://resource.hsslive.cn/image/c762c97e87859eb6d1b15c853a026c6d.webp)
+
 # 本地启动
 
 ## 安装依赖
--- script/config/webpack.common.ts
+++ script/config/webpack.common.ts
@@ -335,35 +335,35 @@ const commonConfig = (isProduction) => {
     plugins: [
       // 友好的显示错误信息在终端
       new FriendlyErrorsWebpackPlugin(),
-      new ForkTsCheckerWebpackPlugin(),
-      // new ForkTsCheckerWebpackPlugin({
-      //   // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin
-      //   typescript: {
-      //     // extensions: {
-      //     //   vue: {
-      //     //     enabled: true,
-      //     //     compiler: resolveApp('./node_modules/vue/compiler-sfc/index.js'),
-      //     //   },
-      //     // },
-      //     diagnosticOptions: {
-      //       semantic: true,
-      //       syntactic: false,
-      //     },
-      //   },
-      //   /**
-      //    * devServer如果设置为false，则不会向 Webpack Dev Server 报告错误。
-      //    * 但是控制台还是会打印错误。
-      //    */
-      //   devServer: false, // 7.x版本：https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/issues/723
-      //   // logger: {
-      //   //   devServer: false, // fork-ts-checker-webpack-plugin6.x版本
-      //   // },
-      //   /**
-      //    * async 为 false，同步的将错误信息反馈给 webpack，如果报错了，webpack 就会编译失败
-      //    * async 默认为 true，异步的将错误信息反馈给 webpack，如果报错了，不影响 webpack 的编译
-      //    */
-      //   async: true,
-      // }),
+      new ForkTsCheckerWebpackPlugin({
+        // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin
+        typescript: {
+          // extensions: {
+          //   vue: {
+          //     enabled: true,
+          //     compiler: resolveApp('./node_modules/vue/compiler-sfc/index.js'),
+          //   },
+          // },
+          memoryLimit: 1024 * 3,
+          diagnosticOptions: {
+            semantic: true,
+            syntactic: false,
+          },
+        },
+        /**
+         * devServer如果设置为false，则不会向 Webpack Dev Server 报告错误。
+         * 但是控制台还是会打印错误。
+         */
+        devServer: false, // 7.x版本：https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/issues/723
+        // logger: {
+        //   devServer: false, // fork-ts-checker-webpack-plugin6.x版本
+        // },
+        /**
+         * async 为 false，同步的将错误信息反馈给 webpack，如果报错了，webpack 就会编译失败
+         * async 默认为 true，异步的将错误信息反馈给 webpack，如果报错了，不影响 webpack 的编译
+         */
+        async: true,
+      }),
       // 解析vue
       new VueLoaderPlugin(),
       // eslint-disable-next-line
